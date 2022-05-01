b22a4b73e27de3029ee8a5698862e6d7d154d655
AlexeyVolovik <alexeyvolovik@gmail.com>
2022-05-01 16:38:44 +0200

    New version.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
 
+enum MenuSelection {
+  Landing = 0,
+  Donate = 1,
+  Volunteer = 2,
+  Partner = 3,
+  Request = 4,
+  Reports = 5,
+  Media = 6,
+  About = 7
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  MenuSelection = MenuSelection;
+
   title = 'Zeilen';
+  menuSelection: MenuSelection = MenuSelection.Landing;
+
+  setMenuSelection(selection: MenuSelection) {
+    this.menuSelection = selection;
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,27 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { LandingComponent } from './landing/landing.component';
+import { DonateComponent } from './donate/donate.component';
+import { VolunteerComponent } from './volunteer/volunteer.component';
+import { PartnerComponent } from './partner/partner.component';
+import { ReportsComponent } from './reports/reports.component';
+import { MediaComponent } from './media/media.component';
+import { AboutComponent } from './about/about.component';
+import { RequestComponent } from './request/request.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    LandingComponent
+    LandingComponent,
+    DonateComponent,
+    VolunteerComponent,
+    PartnerComponent,
+    ReportsComponent,
+    MediaComponent,
+    AboutComponent,
+    RequestComponent
   ],
   imports: [
     BrowserModule
@@ -19,4 +33,4 @@ import { LandingComponent } from './landing/landing.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
