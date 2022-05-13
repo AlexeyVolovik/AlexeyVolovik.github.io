89c6f307ed74c0f1efe0457e16cfc592a9fbcd92
AlexeyVolovik <alexeyvolovik@gmail.com>
2022-05-13 12:37:13 +0200

    New version.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -25,6 +25,9 @@ import { RequestMobileComponent } from './request-mobile/request-mobile.componen
 import { ReportsMobileComponent } from './reports-mobile/reports-mobile.component';
 import { MediaMobileComponent } from './media-mobile/media-mobile.component';
 import { AboutMobileComponent } from './about-mobile/about-mobile.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from "@angular/material/button";
+import {MatMenuModule} from "@angular/material/menu";
 
 @NgModule({
   declarations: [
@@ -54,7 +57,10 @@ import { AboutMobileComponent } from './about-mobile/about-mobile.component';
     AboutMobileComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/header-mobile/header-mobile.component.ts
+++ src/app/header-mobile/header-mobile.component.ts
@@ -1,4 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+
+enum MenuSelection {
+  Landing = 0,
+  Donate = 1,
+  Volunteer = 2,
+  Partner = 3,
+  Request = 4,
+  Reports = 5,
+  Media = 6,
+  About = 7
+}
 
 @Component({
   selector: 'app-header-mobile',
@@ -6,10 +17,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header-mobile.component.css']
 })
 export class HeaderMobileComponent implements OnInit {
+  MenuSelection = MenuSelection;
+
+  @Output() menuChange = new EventEmitter<MenuSelection>();
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  menuButtonClick(menuSelection: MenuSelection) {
+    console.log("menu button clicked");
+
+    this.menuChange.emit(menuSelection);
   }
 
 }
