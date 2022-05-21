84ed40d08e710e6901083488c3ed7b06071b9342
AlexeyVolovik <alexeyvolovik@gmail.com>
2022-05-21 10:50:39 +0200

    New version.

--- src/app/partner-all-mobile/partner-all-mobile.component.ts
+++ src/app/partner-all-mobile/partner-all-mobile.component.ts
@@ -1,6 +1,7 @@
 import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {PartnerTabSelection} from "../enumerators";
 import {MenuService} from "../services/menu.service";
+import {TranslateService} from "@ngx-translate/core";
 
 @Component({
   selector: 'app-partner-all-mobile',
@@ -12,7 +13,10 @@ export class PartnerAllMobileComponent implements OnInit {
 
   selected: PartnerTabSelection = PartnerTabSelection.All;
 
-  constructor(public menuService: MenuService) { }
+  constructor(public menuService: MenuService, private translate: TranslateService) {
+    translate.setDefaultLang('en');
+    translate.use(menuService.language.toLowerCase());
+  }
 
   ngOnInit(): void {
   }
