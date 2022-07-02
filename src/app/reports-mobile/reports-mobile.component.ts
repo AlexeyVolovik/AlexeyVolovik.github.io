c6eeabe6c8eb5ec9367de151a823c7d5b84759be
AlexeyVolovik <alexeyvolovik@gmail.com>
2022-07-02 19:15:02 +0200

    New version.

--- src/app/reports-mobile/reports-mobile.component.ts
+++ src/app/reports-mobile/reports-mobile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ReportsService} from "../services/reports.service";
 
 @Component({
   selector: 'app-reports-mobile',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReportsMobileComponent implements OnInit {
 
-  constructor() { }
+  constructor(public repData: ReportsService) { }
 
   ngOnInit(): void {
   }
